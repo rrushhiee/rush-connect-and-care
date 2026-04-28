c04438efa499240f5b91a8f5b9fcd2d0f98158c0
Rupert Rush <rupert.rush@me.com>
2026-04-28 16:34:50 +1000

    Submit enquiry form directly to FormSubmit

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.RCC_CONFIG = Object.freeze({
-  enquiryEndpoint: "https://formsubmit.co/ajax/hello@rushconnectandcare.com.au",
+  enquiryEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
   bookingEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
   newsletterEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
   formSuccessUrl: "/thank-you.html"
