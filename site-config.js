9dd03059c9b5b709e9e1ede560a93c76ef4453b1
Rupert Rush <rupert.rush@me.com>
2026-04-28 13:23:56 +1000

    Use absolute thank-you redirect for forms

--- site-config.js
+++ site-config.js
@@ -2,5 +2,5 @@ window.RCC_CONFIG = Object.freeze({
   enquiryEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
   bookingEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
   newsletterEndpoint: "https://formsubmit.co/hello@rushconnectandcare.com.au",
-  formSuccessUrl: "thank-you.html"
+  formSuccessUrl: "/thank-you.html"
 });
